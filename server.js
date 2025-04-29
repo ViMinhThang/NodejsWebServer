457c9c3ebb10ea1c0e661b57c4f91371f1c1500d
HuynhMinhThangV <22130251@st.hcmuaf.edu.vn>
2025-04-29 20:36:50 +0700

    coding express-like framework

--- server.js
+++ server.js
@@ -1,63 +1,13 @@
-const http = require("node:http");
-const fs = require("node:fs/promises");
-const server = http.createServer();
+const Butter = require("./butter");
 
-server.on("request", async (request, response) => {
-  if (request.url === "/" && request.method === "GET") {
-    response.setHeader("Content-Type", "text/html");
+const PORT = 4060;
 
-    const fileHandle = await fs.open("./public/index.html", "r");
-    const fileStream = fileHandle.createReadStream();
-    fileStream.pipe(response);
-  }
-  if (request.url === "/styles.css" && request.method === "GET") {
-    response.setHeader("Content-Type", "text/css");
+const server = new Butter();
 
-    const fileHandle = await fs.open("./public/styles.css", "r");
-    const fileStream = fileHandle.createReadStream();
-    fileStream.pipe(response);
-  }
-  if (request.url === "/script.js" && request.method === "GET") {
-    response.setHeader("Content-Type", "text/javascript");
-
-    const fileHandle = await fs.open("./public/script.js", "r");
-    const fileStream = fileHandle.createReadStream();
-    fileStream.pipe(response);
-  }
-  if (request.url === "/login" && request.method === "POST") {
-    response.setHeader("Content-Type", "application/json");
-    response.statusCode = 200;
-
-    const body = {
-      message: "Logging you in ....",
-    };
-
-    response.end(JSON.stringify(body));
-  }
-  if (request.url === "/user" && request.method === "PUT") {
-    response.setHeader("Content-Type", "application/json");
-    response.statusCode = 200;
-
-    const body = {
-      message: "Updating your info...",
-    };
-
-    response.end(JSON.stringify(body));
-  }
-  if (request.url === "/upload" && request.method === "PUT") {
-    const fileHandle = await fs.open("./storage/image.jpeg", "w");
-    const fileStream = fileHandle.createWriteStream();
-    response.setHeader("Content-Type", "application/json");
-
-    request.pipe(fileStream);
-    request.on("end", () => {
-      response.end(
-        JSON.stringify({ message: "File was uploaded successfully!" })
-      );
-    });
-  }
+server.route("get", "/", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
 });
 
-server.listen(9000, () => {
-  console.log("Web server is live at http://localhost:9000");
+server.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
 });
