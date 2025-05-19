d5c520ad86848d27c82e42e4ec61952981628a31
ViMinhThang <huynhminhthang246@github.com>
2025-05-19 11:39:36 +0700

    intital code

---
+++ frontend/vite-video-edit/src/components/Home.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import InlineLoading from "../reusable/InlineLoading";
+import t from "../lib/tokens";
+import Uploader from "./Uploader";
+import { AppContext } from "../App";
+import Videos from "./Videos";
+const Home = () => {
+  const { loggedIn, setLoggedIn, section, setSection } = useContext(AppContext);
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    if (loggedIn === false) {
+      navigate("/login");
+    }
+  }, [loggedIn]);
+
+
+  return (
+    <div className="posts-container">
+      <Uploader />
+      <Videos />
+    </div>
+  );
+};
+
+export default Home;
