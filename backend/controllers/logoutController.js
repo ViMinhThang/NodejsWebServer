75f2fbe4b3f02925ef915ff33c9cec8536553f67
ViMinhThang <huynhminhthang246@gmail.com>
2025-05-23 01:11:09 +0700

    handle token

--- backend/controllers/logoutController.js
+++ backend/controllers/logoutController.js
@@ -1,7 +1,7 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 
 const logout = asyncHandler(async (req, res, next) => {
-  res.cookie("jwt", "", {
+  res.cookie("token", "", {
     httpOnly: true,
     expires: new Date(0),
   });
--- backend/util/generateToken.js
+++ backend/util/generateToken.js
@@ -4,7 +4,7 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
-  res.cookie("jwt", token, {
+  res.cookie("token", token, {
     httpOnly: true,
     secure: true,
     sameSite: "none",
