dd24ea8492654d73813191a5c0ea0952146dd9e9
ViMinhThang <huynhminhthang246@gmail.com>
2025-05-22 18:02:40 +0700

    cors config

--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,26 @@ import userRoutes from "./routes/userRoutes.js";
 import logoutRoutes from "./routes/logoutRoutes.js";
 import videoRoutes from "./routes/videoRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import cors from "cors"
+import cors from "cors";
 const port = process.env.PORT || 5000;
 
 dotenv.config();
-
+const corsOptions = {
+  origin: "https://nodejs-web-server-five.vercel.app",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization", "filename"],
+};
 const startServer = async () => {
   try {
     await connectDB();
 
     const app = express();
-    app.use(cors())
-    app.use(express.json());
+    app.use(cors());
+    app.use(express.json(corsOptions));
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
-
+    app.options("*", cors(corsOptions));
     app.get("/", (req, res) => {
       res.send("API is running");
     });
